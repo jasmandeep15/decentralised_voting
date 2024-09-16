9d4f2bd7b4065d75bcf01db847e00d1011524b69
Jasmandeep <jasmandeep1551999@gmail.com>
2024-09-16 19:45:35 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProposalList from './ProposalList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Decentralized Voting System</h1>
+      <ProposalList />
+    </div>
+  );
+}
+
+export default App;
